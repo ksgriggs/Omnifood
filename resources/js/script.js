127f77bb2bacc85d8ba9e368c67fbc10974cdb01
Scott Griggs <sgriggs@gmail.com>
2021-03-12 15:46:44 -0500

    Initial commit

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
   /* For the sticky navigation */
-  $('.js--section-features').waypoint(
+  $(".js--section-features").waypoint(
     function (direction) {
-      if (direction == 'down') {
-        $('nav').addClass('sticky');
+      if (direction == "down") {
+        $("nav").addClass("sticky");
       } else {
-        $('nav').removeClass('sticky');
+        $("nav").removeClass("sticky");
       }
     },
     {
-      offset: '60px',
+      offset: "60px",
     }
   );
 
   /* Scroll on buttons */
-  $('.js--scroll-to-plan').click(function () {
-    $('html, body').animate(
-      { scrollTop: $('.js--section-plans').offset().top },
+  $(".js--scroll-to-plan").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-plans").offset().top },
       1000
     );
   });
 
-  $('.js--scroll-to-start').click(function () {
-    $('html, body').animate(
-      { scrollTop: $('.js--section-features').offset().top },
+  $(".js--scroll-to-start").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-features").offset().top },
       1000
     );
   });
@@ -38,20 +38,20 @@ $(document).ready(function () {
       .click(function (event) {
         // On-page links
         if (
-          location.pathname.replace(/^\//, '') ==
-            this.pathname.replace(/^\//, '') &&
+          location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
           location.hostname == this.hostname
         ) {
           // Figure out element to scroll to
           var target = $(this.hash);
           target = target.length
             ? target
-            : $('[name=' + this.hash.slice(1) + ']');
+            : $("[name=" + this.hash.slice(1) + "]");
           // Does a scroll target exist?
           if (target.length) {
             // Only prevent default if animation is actually gonna happen
             event.preventDefault();
-            $('html, body').animate(
+            $("html, body").animate(
               {
                 scrollTop: target.offset().top,
               },
@@ -61,11 +61,11 @@ $(document).ready(function () {
                 // Must change focus!
                 var $target = $(target);
                 $target.focus();
-                if ($target.is(':focus')) {
+                if ($target.is(":focus")) {
                   // Checking if the target was focused
                   return false;
                 } else {
-                  $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                  $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
                   $target.focus(); // Set focus again
                 }
               }
@@ -74,4 +74,52 @@ $(document).ready(function () {
         }
       });
   });
+
+  /* Animation on scroll */
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass("animate__animated animate__fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass("animate__animated animate__fadeInUp");
+    },
+    {
+      offset: "50%",
+    }
+  );
+  $(".js--wp-3").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass("animate__animated animate__fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+  $(".js--wp-4").waypoint(
+    function (direction) {
+      $(".js--wp-4").addClass("animate__animated animate__pulse");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--header").waypoint(
+    function (direction) {
+      if (direction == "up") {
+        $(".js--wp-1").removeClass("animate__animated animate__fadeIn");
+        $(".js--wp-2").removeClass("animate__animated animate__fadeInUp");
+        $(".js--wp-3").removeClass("animate__animated animate__fadeIn");
+        $(".js--wp-4").removeClass("animate__animated animate__pulse");
+      }
+    },
+    {
+      offset: "-25%",
+    }
+  );
 });
